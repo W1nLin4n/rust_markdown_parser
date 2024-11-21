cc5b7d40833d4afaa538337e9814f1b7d8f10477
W1nLin4n <grechkaartema@gmail.com>
2024-11-21 09:21:29 +0200

    Added ability to work with cli

--- src/main.rs
+++ src/main.rs
@@ -1,31 +1,75 @@
 mod parser;
+use std::{fs, io::{self, Read}, process::Output};
+
 use anyhow::Result;
-use parser::*;
+use clap::{Arg, Command};
+use parser::{markdown_file_to_html, markdown_to_html};
 
 fn main() -> Result<()> {
-    let markdown = r#"
-# Heading 1
-## Heading 2
+    let matches = Command::new("Markdown Parser")
+    .author("Artem Hrechka, grechkaartema@gmail.com")
+    .about("Converts markdown into HTML.")
+    .subcommand(
+        Command::new("help")
+        .about("Displays help information for this program")
+    )
+    .subcommand(
+        Command::new("credits")
+        .about("Displays credists information for this program")
+    )
+    .subcommand(
+        Command::new("parse")
+        .about("Parses markdown into HTML")
+        .arg(
+            Arg::new("input")
+            .help("Input file(default: standard input)")
+            .short('i')
+            .long("input")
+            .required(false)
+            .default_value("-")
+        )
+        .arg(
+            Arg::new("output")
+            .help("Output file(default: standard output)")
+            .short('o')
+            .long("output")
+            .required(false)
+            .default_value("-")
+        )
+    ).get_matches();
+    
+    match matches.subcommand() {
+        Some(("help", _)) => {
+            print!(
+                "Commands:\n\
+                help: This message\n\
+                credits: Info about creator\n\
+                parse: Parse markdown into HTML"
+            )
+        }
+        Some(("credits", _)) => {
+            print!("Artem Hrechka, grechkaartema@gmail.com")
+        }
+        Some(("parse", args)) => {
+            let input_path = args.get_one::<String>("input").unwrap();
+            let output_path = args.get_one::<String>("output").unwrap();
 
----
-
-This is a paragraph with **bold text** and _italic text_.
-
-- List item 1
-- List item 2
-
-1. List item 3
-2. List item 4
-
-[Link](https://example.com)
-
-```
-Code block content
-print("Hello world!")
-```
-"#;
-    let html = markdown_to_html(markdown.to_string())?;
-
-    println!("{}", html);
+            let mut html = String::new();
+            if input_path == "-" {
+                let mut markdown = String::new();
+                io::stdin()
+                    .read_to_string(&mut markdown)?;
+                html = markdown_to_html(markdown)?;
+            } else {
+                html = markdown_file_to_html(input_path.to_string())?;
+            }
+            if output_path == "-" {
+                println!("{}", html);
+            } else {
+                fs::write(output_path, html)?;
+            }
+        }
+        _ => print!("Invalid command. To get a list of commands use `help`")
+    }
     Ok(())
 }
